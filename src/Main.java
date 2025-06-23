a66c21622ace837eb775eb3f46620d49121f62ed
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-23 13:46:25 +0900

    Annotation Deprecated

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,29 @@
-class Animal {
-    public void sound() {
-        System.out.println("Animal makes a sound.");
+class Calculator{
+    /**
+     * @deprecated Use {@link #addNumber(int, int)} instead.
+     */
+    @Deprecated
+    public int add(int a, int b){
+        return a + b;
     }
-}
 
-class Dog extends Animal{
-    @Override
-    public void sound(){
-        System.out.println("Dog barks.");
+    /**
+     *
+     * 두 수를 더하는 메서드
+     * @param a 첫 번째 정수
+     * @param b 두 번째 정수
+     * @return 두 수의 합
+     */
+    public int addNumber(int a, int b){
+        return a + b;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        Animal dog = new Dog();
+        Calculator calculator = new Calculator();
 
-        dog.sound();
+        System.out.println(calculator.add(1,2));
+        System.out.println(calculator.addNumber(2,4));
     }
 }
