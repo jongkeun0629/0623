4c1719bb18b25c29d665ef2053fcb37b8e88c78f
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-23 14:09:46 +0900

    Annotation SuppressWarnings

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,12 @@
-class Calculator{
-    /**
-     * @deprecated Use {@link #addNumber(int, int)} instead.
-     */
-    @Deprecated
-    public int add(int a, int b){
-        return a + b;
-    }
-
-    /**
-     *
-     * 두 수를 더하는 메서드
-     * @param a 첫 번째 정수
-     * @param b 두 번째 정수
-     * @return 두 수의 합
-     */
-    public int addNumber(int a, int b){
-        return a + b;
-    }
-}
-
 public class Main {
+    @SuppressWarnings("rawtypes")
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
+        java.util.List list = new java.util.ArrayList();
 
-        System.out.println(calculator.add(1,2));
-        System.out.println(calculator.addNumber(2,4));
+        list.add("Hello");
+        list.add("World");
+
+        for (Object obj: list)
+            System.out.println(obj);
     }
 }
